22adb327b348a94be2075f1254f4f71cb0b861cb
Lucas <lucas@maisondunet.com>
2017-06-13 13:32:41 +0200

    add css + picto png

--- game-controller.js
+++ game-controller.js
@@ -4,9 +4,8 @@ $(document).ready( function() {
     //SCORE
     var score = 0;
     setInterval(function () {
-        $('#score').html('Score :' + score);
+        $('#score').html('Score :' +' '+ score);
         score++;
     }, 100);
-
 
 });
