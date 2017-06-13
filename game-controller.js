e10e2689b285d816819f89e0f3db03b063f145a7
nicolas alexandropoulos <nicolas.alexandropoulos@univ-grenoble-alpes.fr>
2017-06-13 15:16:02 +0200

    added startgame function

--- game-controller.js
+++ game-controller.js
@@ -1,12 +1,41 @@
+var running = 0;
+//Lancer le jeu
+function startGame(){
+    //On lance le jeu
+    if($('#SpaceShip').length == 0){
+        $('.canvas').append('<canvas id="SpaceShip"></canvas>');
+    }
+    requestAnimFrame(tick);
+    //SCORE
+    running = 1;
+    var score = 0;
+    setInterval(function () {
+        if(running == 1){
+            $('#score').html('Score :' + score);
+            score++;
+        }
+    }, 100);
+}
+
+function stopGame(){
+
+    //TODO: ajouter div par dessus canvas
+
+    //On interrompt le score
+    running = 0;
+}
+
+function resetGame() {
+
+}
 
 $(document).ready( function() {
 
-    //SCORE
-    var score = 0;
-    setInterval(function () {
-        $('#score').html('Score :' + score);
-        score++;
-    }, 100);
+
+
 
 
 });
+
+
+
